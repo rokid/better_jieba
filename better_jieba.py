df9602fb9f5a103a5196d26b3d104bf7a85db746
Tong Guo <779222056@qq.com>
2018-06-19 16:12:50 +0800

    Update and rename example.py to better_jieba.py

--- better_jieba.py
+++ better_jieba.py
@@ -1,18 +1,18 @@
 # coding=UTF-8
 import jieba
-jieba.add_word("我的新词",freq=999999)
 
-def fenci(one_string):
+def isAllZh(s):# 判断是否全是中文
+    for c in s:
+        if not ('\u4e00' <= c <= '\u9fa5'):
+            return False
+    return True
+
+def fenci_1(one_string):
     for _ in range(len(one_string)): # 去掉所有空格
         try:
             one_string=one_string.replace(" ","")
         except:
             break
-    def isAllZh(s): # 判断是否全是中文
-        for c in s:
-            if not ('\u4e00' <= c <= '\u9fa5'):
-                return False
-        return True
     final_result = []
     temp_list = jieba.lcut(one_string)
     for word in temp_list:
@@ -26,6 +26,25 @@ def fenci(one_string):
             or (len(word)==3 and jieba.get_FREQ(word)!=None and jieba.get_FREQ(word)<100):
             jieba.del_word(word) # 强制
             final_result.extend(jieba.lcut(word))
+        else:
+            final_result.append(word)
+    return final_result
+
+# HMM=False已实际使之缩小了不少粒度
+def fenci_2(one_string):
+    for _ in range(len(one_string)):
+        try:
+            one_string = one_string.replace(" ", "")
+        except:
+            break
+    temp_list = jieba.lcut(one_string,HMM=False)
+    final_result = []
+    for word in temp_list:
+        if isAllZh(word) == False:
+            continue
+        if len(word) > 3:
+            jieba.del_word(word)
+            final_result.extend(jieba.lcut(word,HMM=False))
         else:
             final_result.append(word)
     return final_result
